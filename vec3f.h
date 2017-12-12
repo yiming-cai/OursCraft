f79e7e93316946482d5e6182c089f39fca0abb3a
Yue Pan <robertpanyue@gmail.com>
2017-12-11 16:25:00 -0800

    add particle effect 50%done

---
+++ vec3f.h
@@ -0,0 +1,39 @@
+/* File for "Particle Systems" lesson of the OpenGL tutorial on
+ * www.videotutorialsrock.com
+ */
+#ifndef VEC3F_H_INCLUDED
+#define VEC3F_H_INCLUDED
+
+#include <iostream>
+
+class Vec3f {
+	private:
+		float v[3];
+	public:
+		Vec3f();
+		Vec3f(float x, float y, float z);
+		
+		float &operator[](int index);
+		float operator[](int index) const;
+		
+		Vec3f operator*(float scale) const;
+		Vec3f operator/(float scale) const;
+		Vec3f operator+(const Vec3f &other) const;
+		Vec3f operator-(const Vec3f &other) const;
+		Vec3f operator-() const;
+		
+		const Vec3f &operator*=(float scale);
+		const Vec3f &operator/=(float scale);
+		const Vec3f &operator+=(const Vec3f &other);
+		const Vec3f &operator-=(const Vec3f &other);
+		
+		float magnitude() const;
+		float magnitudeSquared() const;
+		Vec3f normalize() const;
+		float dot(const Vec3f &other) const;
+		Vec3f cross(const Vec3f &other) const;
+};
+
+Vec3f operator*(float scale, const Vec3f &v);
+std::ostream &operator<<(std::ostream &output, const Vec3f &v);
+#endif
