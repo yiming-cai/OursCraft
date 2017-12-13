2651fd0715ca9ba7a632934b9240bb0e437e2152
Madaoism <cai_yiming@yahoo.com>
2017-12-13 04:47:59 -0800

    thickened silhouette

--- Tree.cpp
+++ Tree.cpp
@@ -16,3 +16,68 @@ void Tree::setSeed(int in)
 	seed = in;
 	srand(seed);
 }
+
+void Tree::createCylinder(glm::vec3 p1, glm::vec3 p2, float thickness)
+{
+	// direction serves as a normal
+	glm::vec3 direction = glm::normalize(p2 - p1);
+	int min = direction[0];
+	int index = 0;
+	for (int i = 1; i < 3; i++)
+	{
+		if (direction[i] < min)
+		{
+			index = i;
+			min = direction[i];
+		}
+	}
+
+	glm::vec3 cross_vec(0.0f);
+	cross_vec[index] = 1.0f;
+
+	glm::vec3 product1 = glm::cross(direction, cross_vec);
+	glm::vec3 product2 = glm::cross(product1, direction);
+
+	int offset = vertices.size();
+	vertices.push_back(p1 + product1*thickness / 2.0f); normals.push_back(product1);
+	vertices.push_back(p1 + product2*thickness / 2.0f); normals.push_back(product2);
+	vertices.push_back(p1 - product1*thickness / 2.0f); normals.push_back(-1.0f*product1);
+	vertices.push_back(p1 - product2*thickness / 2.0f); normals.push_back(-1.0f*product2);
+
+	vertices.push_back(p2 + product1*thickness / 2.0f); normals.push_back(product1);
+	vertices.push_back(p2 + product2*thickness / 2.0f); normals.push_back(product2);
+	vertices.push_back(p2 - product1*thickness / 2.0f); normals.push_back(-1.0f*product1);
+	vertices.push_back(p2 - product2*thickness / 2.0f); normals.push_back(-1.0f*product2);
+
+	std::vector<glm::ivec3> temp;
+	temp.push_back(glm::ivec3(0, 4, 5));
+	temp.push_back(glm::ivec3(5, 1, 0));
+	temp.push_back(glm::ivec3(1, 5, 6));
+	temp.push_back(glm::ivec3(6, 2, 1));
+	temp.push_back(glm::ivec3(2,6,7));
+	temp.push_back(glm::ivec3(7,3,2));
+	temp.push_back(glm::ivec3(3,7,4));
+	temp.push_back(glm::ivec3(4, 0, 3));
+
+	for (int i = 0; i < temp.size(); i++)
+	{
+		indices.push_back( (temp[i] + offset) );
+		colors.push_back( curr_color );
+	}
+}
+
+void Tree::initArrays()
+{
+}
+
+void Tree::render(GLuint shaderProgram)
+{
+}
+
+Branch::Branch(int seed, Branch * parent)
+{
+}
+
+Branch::~Branch()
+{
+}
--- Tree.h
+++ Tree.h
@@ -1,16 +1,44 @@
 #pragma once
 #include "Util.h"
 
+const static glm::vec3 brown = glm::vec3(0.4f, 0.2f, 0);
+const static glm::vec3 green = glm::vec3(0.4f, 0.6f, 0);
+
+class Branch
+{
+private:
+public:
+
+	Branch * parent;
+	std::vector<Branch *> branches;
+
+	Branch(int seed, Branch * parent);
+	~Branch();
+};
+
+
 class Tree
 {
 private:
 
 	int seed = 15;
+	Branch * root;
+	glm::vec3 curr_color;
+
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec3> normals;
+	std::vector<glm::ivec3> indices;
+	std::vector<glm::vec3> colors;
+
+	GLuint VAO, VBO, NBO, CBO, EBO;
 
 public:
 	Tree();
 	~Tree();
 
 	void setSeed(int);
+	void createCylinder(glm::vec3 p1, glm::vec3 p2, float thickness);
+	void initArrays();
+	void render(GLuint shaderProgram);
 };
 
